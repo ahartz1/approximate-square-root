47913deb00c97d8a32108dc1d59b4636e4251935
Adam Hartz <ahartz1@gmail.com>
2015-09-01 16:06:47 -0400

    Added error messsages.

--- square_root.py
+++ square_root.py
@@ -21,5 +21,17 @@ def newton_sq_rt(root_of):
         n += 1
     return apxroot
 
-root_of = float(input("Enter a positive number: "))
+def get_input():
+    root_of = input("Enter a positive number: ")
+    try:
+        float(root_of)
+    except:
+        print("Non-numeric input.".format(end=''))
+        return get_input()
+    if float(root_of) < 0:
+        print("Negative numbers not allowed. ".format(end=''))
+        return get_input()
+    return float(root_of)
+
+root_of = get_input()
 print("The square root of {} is {}".format(root_of, newton_sq_rt(root_of)))
