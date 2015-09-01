8577d229ddf4288f51dddb4ebd36a3ebf8bbbd5d
Adam Hartz <ahartz1@gmail.com>
2015-09-01 15:43:24 -0400

    Working 'easy' version of square_root.py

--- square_root.py
+++ square_root.py
@@ -2,20 +2,20 @@
 1. Have your program ask the user for a positive number and
     store it in a variable
 2. Have your program perform Newton's method of successive approximations to
-    approximate the square root of the inputted number'''
+    approximate the square root of the inputted number
 
-'''
+
 Notes: You have to start somewhere, but where?
 '''
 
+
 def newton_sq_rt(root_of):
-    apxroot = []
-    apxroot.append(root_of/2)
+    apxroot = root_of/2
     n = 0
-    while apxroot[n] - (root_of/apxroot[n]) > 0.01:
-        apxroot.append(apxroot[n] - (root_of/apxroot[n]))
+    while abs(root_of - apxroot**2) > 0.01:
+        apxroot  = (apxroot + root_of/apxroot)/2
         n += 1
-    return apxroot[len(apxroot)-1]
+    return apxroot
 
 root_of = float(input("Enter a positive number: "))
 print("The square root of {} is {}".format(root_of, newton_sq_rt(root_of)))
