fe6d6fb5ad6f7036f51a9e2318f68e5edc7589c9
Adam Hartz <ahartz1@gmail.com>
2015-09-01 15:00:02 -0400

    Basic structure in place. Next: which value to start with?

--- square_root.py
+++ square_root.py
@@ -4,8 +4,18 @@
 2. Have your program perform Newton's method of successive approximations to
     approximate the square root of the inputted number'''
 
+'''
+Notes: You have to start somewhere, but where?
+'''
+
 def newton_sq_rt(root_of):
-    pass
+    apxroot = []
+    apxroot.append(root_of/2)
+    n = 0
+    while apxroot[n] - (root_of/apxroot[n]) > 0.01:
+        apxroot.append(apxroot[n] - (root_of/apxroot[n]))
+        n += 1
+    return apxroot[len(apxroot)-1]
 
 root_of = float(input("Enter a positive number: "))
 print("The square root of {} is {}".format(root_of, newton_sq_rt(root_of)))
