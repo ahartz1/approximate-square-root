894a7cf6f180edc8cffb3ed5bd004552cf45b9d3
Adam Hartz <ahartz1@gmail.com>
2015-09-01 14:44:18 -0400

    Setup basic structure

---
+++ square_root.py
@@ -0,0 +1,11 @@
+'''
+1. Have your program ask the user for a positive number and
+    store it in a variable
+2. Have your program perform Newton's method of successive approximations to
+    approximate the square root of the inputted number'''
+
+def newton_sq_rt(root_of):
+    pass
+
+root_of = float(input("Enter a positive number: "))
+print("The square root of {} is {}".format(root_of, newton_sq_rt(root_of)))
